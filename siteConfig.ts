a06b28484048ec8800e5808f21fd775fb79b2666
224617 <sunsai4617@gmail.com>
2026-08-13 06:28:42 +0800

    Auto sync source code for Vercel 🚀

--- siteConfig.ts
+++ siteConfig.ts
@@ -48,9 +48,9 @@ export const siteConfig = {
   // 👇 【新增】：全局背景弹幕配置
   danmakuList: ["在干嘛呢？", "有笨蛋嘛？", "前方高能反应！", "GROMACS 跑起来了吗？", "MD 模拟什么时候才能出图啊", "Graph Neural Networks 炼丹中...", "BUG 修复进度 99%", "今天背单词了吗？", "Tailwind CSS 拯救前端", "写算法中", "睡大觉中", "到底在干嘛？"],
   gitalkConfig: {
-    clientID: "",
-    clientSecret: "",
-    repo: "SaichaoSun",
+    clientID: "Ov23li9YpQq5e4xMX2Vb",
+    clientSecret: "bb362c11d7fc9cc826781bb23abb7fa7a0f75fc6",
+    repo: "Saichao-Sun",
     owner: "224617",
     admin: ["224617"],
   },
