8245eb02c1d885eb353679dc2ef5962f6d690b18
224617 <sunsai4617@gmail.com>
2026-08-14 02:07:11 +0800

    Auto sync source code for Vercel 🚀

--- app/about/about.md
+++ app/about/about.md
@@ -1,6 +1,6 @@
 ---
 title: 关于我
-date: '2026-03-31'
+date: '2026-03-31 22:28:25'
 tags: []
 mood: ''
 cover: https://bu.dusays.com/2026/03/24/69c23dc278c78.jpg
@@ -9,7 +9,7 @@ description: ''
 
 个人简介
 
-你好，我是 XingHuiSama。
+你好，我是 SunSaiChai。
 
 专注于**计算化学、人工智能与软件工程**的交叉应用。
 
--- data/albums.ts
+++ data/albums.ts
@@ -4,12 +4,107 @@ export interface Album { id: string; title: string; description: string; cover: 
 
 export const albums: Album[] = [
   {
+    "title": "扫雷",
+    "cover": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/OIP.jpg",
+    "id": "album_1786638394639",
+    "photos": [
+      {
+        "caption": "2311",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-08-11%20015311.png?raw=true"
+      },
+      {
+        "caption": "2004",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-08-03%20003202.png?raw=true"
+      },
+      {
+        "caption": "1258",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-19%20025451.png?raw=true"
+      },
+      {
+        "caption": "1093",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-19%20003445.png?raw=true"
+      },
+      {
+        "caption": "991",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-19%20001220.png?raw=true"
+      },
+      {
+        "caption": "844",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-18%20060948.png?raw=true"
+      },
+      {
+        "caption": "873",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-18%20055111.png?raw=true"
+      },
+      {
+        "caption": "753",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-18%20050118.png?raw=true"
+      },
+      {
+        "caption": "686",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-18%20043107.png?raw=true"
+      },
+      {
+        "caption": "579",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-18%20041424.png?raw=true"
+      },
+      {
+        "caption": "544",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-18%20034126.png?raw=true"
+      },
+      {
+        "caption": "476",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-17%20011800.png?raw=true"
+      },
+      {
+        "caption": "99",
+        "url": "https://github.com/224617/Saichao-Sun/blob/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-17%20004436.png?raw=true"
+      },
+      {
+        "caption": "40",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-17%20004016.png"
+      },
+      {
+        "caption": "10",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-07-17%20003543.png"
+      },
+      {
+        "caption": "587",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/1786637796900.jpg"
+      },
+      {
+        "caption": "727",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/1786637796898.jpg"
+      },
+      {
+        "caption": "940",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/1786637796896.jpg"
+      },
+      {
+        "caption": "1072",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/1786637796893.jpg"
+      },
+      {
+        "caption": "1340",
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/1786637796891.jpg"
+      },
+      {
+        "url": "https://raw.githubusercontent.com/224617/Saichao-Sun/refs/heads/main/images/1786637796889.png",
+        "caption": "2445"
+      }
+    ],
+    "date": "2026-08-13"
+  },
+  {
     "id": "terra-journey",
     "title": "泰拉大陆纪行",
     "description": "关于源石、孤星与前文明的视觉记录（测试用相册）",
     "cover": "https://bu.dusays.com/2026/03/24/69c24230de927.jpg",
     "date": "2026.01",
     "photos": [
+      {
+        "url": "扫雷"
+      },
       {
         "url": "https://bu.dusays.com/2026/03/31/69cb69bb530d8.jpg",
         "caption": "原来的人"
